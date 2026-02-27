ddba6f3d1fb13caeb08270c05c2ee2d22a9e33fc
lxuan1997 <lxuan1997@github.com>
2026-02-27 10:29:48 +0800

    feat: 添加认证基础URL配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@sidebase/nuxt-auth'],
   auth: {
+    baseURL: process.env.AUTH_ORIGIN,
     provider: {
       type: 'authjs'
     }
